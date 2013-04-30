2522abc6b51848f25251eb0c3b019cb6ed6a196b
Savannah Niles <savannahniles@gmail.com>
2013-04-30 09:45:14 -0500

    Got rid of bin and gen

--- com.final_project.Niles/gen/com/example/com/final_project/niles/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.com.final_project.niles;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
