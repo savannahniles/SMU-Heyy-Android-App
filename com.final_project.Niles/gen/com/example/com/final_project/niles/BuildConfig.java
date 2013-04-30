3e44fc513321e0643230561e0e4c5efb9800eced
Savannah Niles <savannahniles@gmail.com>
2013-04-30 09:43:02 -0500

    Changed everything

---
+++ com.final_project.Niles/gen/com/example/com/final_project/niles/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.com.final_project.niles;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
