c89e2e1aaa7da5e992eca9f16ae7df1745bae5ac
Savannah Niles <savannahniles@gmail.com>
2013-04-30 13:29:45 -0500

    Made new activity that launches with list activity is clicked

--- com.final_project.Niles/src/com/example/com/final_project/niles/MainActivity.java
+++ com.final_project.Niles/src/com/example/com/final_project/niles/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.com.final_project.niles;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends ListActivity {
 
@@ -16,7 +22,26 @@ public class MainActivity extends ListActivity {
          
         // Binding resources Array to ListAdapter
         this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.label, posts));
-         
+        
+        ListView lv = getListView();
+        
+        // listening to single list item on click
+        lv.setOnItemClickListener(new OnItemClickListener() {
+          public void onItemClick(AdapterView<?> parent, View view,
+              int position, long id) {
+               
+              // selected item 
+              String product = ((TextView) view).getText().toString();
+               
+              // Launching new Activity on selecting single List Item
+              Intent i = new Intent(getApplicationContext(), SingleListItem.class);
+              // sending data to new activity
+              i.putExtra("product", product);
+              startActivity(i);
+             
+          }
+        });
+        
 	}
 
 	@Override
