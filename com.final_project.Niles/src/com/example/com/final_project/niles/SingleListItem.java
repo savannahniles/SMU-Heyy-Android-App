b126a0a4e3cc772152d8102c1fd9c8050e93d682
Savannah Niles <savannahniles@gmail.com>
2013-05-02 21:05:47 -0500

    Google maps API permissions set up, map activity added and launched from selecting a post

--- com.final_project.Niles/src/com/example/com/final_project/niles/SingleListItem.java
+++ com.final_project.Niles/src/com/example/com/final_project/niles/SingleListItem.java
@@ -1,23 +1,21 @@
 package com.example.com.final_project.niles;
 
-import android.app.Activity;
-import android.content.Intent;
+import com.google.android.gms.maps.MapView;
+import com.google.android.maps.MapActivity;
+
 import android.os.Bundle;
-import android.widget.TextView;
  
-public class SingleListItem extends Activity{
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.single_list_item_view);
-         
-        TextView txtPost = (TextView) findViewById(R.id.single_post_label);
-         
-        Intent i = getIntent();
-        // getting attached intent data
-        String post = i.getStringExtra("post");
-        // displaying selected product name
-        txtPost.setText(post);
-         
-    }
+public class SingleListItem extends MapActivity{
+	@Override
+	protected boolean isRouteDisplayed() {
+	    return false;
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main);
+	    MapView mapView = (MapView) findViewById(R.id.mapview);
+	    mapView.setBuiltInZoomControls(true);
+	}
 }
